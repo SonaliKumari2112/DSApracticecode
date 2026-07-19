05207ff5a99446726ebae0542647bc083b3454d4
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-07-19 15:09:46 +0530

    secondLargestSmallest

--- ac_codding_practice/secondLargestSmallest.cpp
+++ ac_codding_practice/secondLargestSmallest.cpp
@@ -19,14 +19,22 @@ for(int i=0;i<n;i++)
     if(v[i]<small){
        small2=small;
        small=v[i];
-    }
+    }else if (v[i] < small2 && v[i] != small) {
+            small2 = v[i];
+        }
     
     if(v[i]>large)
 {
     large2=large;
     large=v[i];
-}}
+}else if (v[i] > large2 && v[i] != large) {
+            large2 = v[i];
+        }
+}
 cout<<"smallest ele-->"<<small<<endl;
-cout<<"2nd smallest ele-->"<<small2;
+cout<<"2nd smallest ele-->"<<small2<<endl;
+cout<<"largest element -->"<<large<<endl;
+cout<<"2nd largest element -->"<<large2;
+
    return 0;
 }
