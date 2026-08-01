2b0cbf56baa1f2fb7ee151c7cff5df6d87506bfd
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-08-02 00:31:26 +0530

    substringcheck

---
+++ ac_codding_practice/substringcheck.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+   string s;
+   cin>>s;
+   string sub;
+   cin>>sub;
+    string ss=s+s;
+   if (ss.find(sub) != string ::npos){
+    cout<<"substring found";
+   }else{
+    cout<<"substring not found";
+   }
+   
+
+
+    return 0;
+}
