d74de06d17cab292d84a9ce821e927aa26113745
SonaliKumari2112 <sonalisharma211206@gmail.com>
2026-08-03 20:32:46 +0530

    reverse_order_word_sentence

--- ac_codding_practice/reverse_order_word_sentence.cpp
+++ ac_codding_practice/reverse_order_word_sentence.cpp
@@ -5,16 +5,20 @@ using namespace std;
 int main(){
     string s,word;
     getline(cin,s);
+
     stack<string>st;
     stringstream ss(s);
+
     while(ss>>word){
         st.push(word);
     }
+
     for(int i=st.size();i>=0;i++){
         string n=st.top();
         st.pop();
     cout<<n<<" ";
 
     }
+    
     return 0;
 }
